b4e3e490353c07ecfe9f7d741f04d63349936642
Andreas203 <apapakonstantinou95@gmail.com>
2019-11-18 20:45:12 +0000

    maps added to new visit

--- app/src/main/java/com/example/geoto/ui/main/NewVisitFragment.java
+++ app/src/main/java/com/example/geoto/ui/main/NewVisitFragment.java
@@ -6,20 +6,26 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.geoto.PageViewModel;
 import com.example.geoto.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class NewVisitFragment extends Fragment {
+public class NewVisitFragment extends Fragment implements OnMapReadyCallback {
 
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     private PageViewModel pageViewModel;
+    private MapView mapView;
+    private GoogleMap googleMap;
 
     public static NewVisitFragment newInstance(int index) {
         NewVisitFragment fragment = new NewVisitFragment();
@@ -41,11 +47,25 @@ public class NewVisitFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapView = (MapView) view.findViewById(R.id.map);
+        mapView.onCreate(savedInstanceState);
+        mapView.onResume();
+        mapView.getMapAsync(this);//when you already implement OnMapReadyCallback in your fragment
+    }
+
+    @Override
     public View onCreateView(
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.new_visit_main, container, false);
 
         return root;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
     }
 }

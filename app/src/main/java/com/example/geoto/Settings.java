884f0ec4a7fe5233716805f8a7b98996553d6a23
evansjames <jamesevans963@gmail.com>
2019-12-15 18:19:30 +0000

    button

--- app/src/main/java/com/example/geoto/Settings.java
+++ app/src/main/java/com/example/geoto/Settings.java
@@ -12,8 +12,5 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        Switch toggle = findViewById(R.id.switch1);
-
     }
 }

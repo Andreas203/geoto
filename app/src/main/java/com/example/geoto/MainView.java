859e7beb273afc3ee81b05798a63e70fd525c05a
Andreas203 <apapakonstantinou95>
2019-12-19 17:18:10 +0000

    removed comments in main view

--- app/src/main/java/com/example/geoto/MainView.java
+++ app/src/main/java/com/example/geoto/MainView.java
@@ -84,30 +84,12 @@ public class MainView extends AppCompatActivity {
 
     }
 
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//
-//
-//
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            System.out.println("The settings button has been pressed!");
-//            startActivity(new Intent(this, Settings.class));
-//            return true;
-//
-//        }
 
         return super.onOptionsItemSelected(item);
     }
